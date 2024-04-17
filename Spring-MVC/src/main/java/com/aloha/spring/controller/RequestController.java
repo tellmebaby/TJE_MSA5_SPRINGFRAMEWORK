91a70e430ecb6aa48eb5912dbc609d6febf4b0ec
tellmebaby <tellmebaby407@gmail.com>
2024-04-17 18:11:50 +0900

    20240417-1811 springMVC(각종 매핑)

--- Spring-MVC/src/main/java/com/aloha/spring/controller/RequestController.java
+++ Spring-MVC/src/main/java/com/aloha/spring/controller/RequestController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -58,4 +59,93 @@ public class RequestController {
 		return "SUCCESS - no : " + no;
 	}
 	
+	/**
+	 * 파라미터 매핑
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/board", method = RequestMethod.GET, 
+					params = "id")
+	public String requestParams(@RequestParam String id) {
+		logger.info("[GET] - /request/board?id=board_123");
+		logger.info("id : " + id);
+		
+		return "request/board";
+		
+	}
+	
+	/**
+	 * 헤더 매핑
+	 * @return
+	 */
+	// headers = "헤더명=값"
+	@ResponseBody
+	@RequestMapping(value = "/board", method = RequestMethod.POST
+					,headers = "Content-Type=application/json")
+	public String requestHeader() {
+		logger.info("[POST] - /request/board");
+		logger.info("헤더 매핑...");
+		return "SUCCESS";
+	}
+	
+	
+	
+	/*
+	 * @ResponseBody O : return "데이터"; ---> 응답메시지(본문: 데이터)
+	 * @ResponseBody X : return "화면이름"; ---> 뷰 리졸버가 jsp 선택->렌더링->html 응답
+	 */
+	@RequestMapping(value = "/board", method = RequestMethod.PUT)
+	public String requestPut() {
+		logger.info("[POST] (PUT) - /request/board");
+		logger.info("헤더 매핑...");
+		return "redirect:/";
+	}
+	
+	
+	/**
+	 * 컨텐츠 타입 매핑
+	 * - Content-Type 헤더의 값으로 매핑
+	 * - consumes = "컨텐츠타입값"
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/board", method = RequestMethod.POST
+					,consumes = "application/xml")
+	public String requestContentType() {
+		logger.info("[POST] - /board/request");
+		logger.info("컨텐츠 타입 매핑...");
+		return "SUCCESS";
+	}
+	
+
+	/**
+	 * Accept 매핑
+	 * - Accept 헤더의 값으로 매핑 (Accept 헤더는 응답 답을 컨텐츠 타입을 지정)
+	 * - consumes = "컨텐츠타입값"
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/board", method = RequestMethod.POST
+					,produces = "application/json")
+	public String requestAccept() {
+		logger.info("[POST] - /board/request");
+		logger.info("Accept 매핑...");
+		return "SUCCESS";
+	}
+	
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Spring-MVC/src/main/webapp/WEB-INF/views/request/board.jsp
+++ Spring-MVC/src/main/webapp/WEB-INF/views/request/board.jsp
@@ -8,9 +8,23 @@
 </head>
 <body>
 	<h1>/request/board</h1>
+	
+	<h3>POST 요청</h3>
 	<form action="<%= request.getContextPath() %>/request/board" method="post">
 		<input type="text" name="no" placeholder="번호 입력" />
 		<input type="submit" value="등록">
 	</form>
+	
+	<h3>PUT 요청</h3>
+	<form action="<%= request.getContextPath() %>/request/board" method="post">
+		<!-- web.xml HiddenHttpMethodFilter 등록 -->
+		<!-- _method 를 HTTP-Method-Override 헤더로 지정한다. -->
+		<!-- X-HTTP-Method-Override 헤더 - 값 : PUT -->
+		<input type="hidden" name="_method" value="PUT" />
+		<input type="text" name="no" placeholder="번호 입력" />
+		<input type="submit" value="등록">
+	</form>
+	
+	
 </body>
 </html>
